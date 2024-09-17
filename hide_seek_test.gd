06b8b928630c879ee123eb396aae6f86248f2f9e
ChikinNoodleSupp <albin.westberg01@gmail.com>
2024-09-17 13:35:36 +0200

    i ddi stuff

--- hide_seek_test.gd
+++ hide_seek_test.gd
@@ -4,6 +4,14 @@ extends Node2D
 var array1 = [1, 2, 3, 4, 5, 6]
 var array2 = [1, 2, 3, 4, 5, 6]
 
+# Declare booleans
+var LolloFound = false
+var BernieFound = false
+
+
+
+
+
 func _on_roll_button_pressed() -> void:
 	 # Roll a random number from array1
 	var random_num1 = get_random_from_array(array1)
@@ -19,13 +27,36 @@ func _on_roll_button_pressed() -> void:
 	# You can now use both random_num1 and random_num2 as needed
 	print("First random number from array1: ", random_num1)
 	print("Second random number from array2 (after removal): ", random_num2)
+	
+	# Check if both LolloFound and BernieFound are true
+	if LolloFound and BernieFound:
+		reset_arrays()
+
 
 # Helper function to get a random number from an array
 func get_random_from_array(arr):
 	# Get a random index within the bounds of the array
 	var random_index = randi() % arr.size()
 	return arr[random_index]
+	
+# Reset function to reinitialize the arrays
+func reset_arrays():
+	# Reset array1 and array2 to their original state
+	array1 = [1, 2, 3, 4, 5, 6]
+	array2 = [1, 2, 3, 4, 5, 6]
+	# Reset booleans if needed
+	LolloFound = false
+	BernieFound = false
+	print("Arrays reset!")
 
 # Make sure to call randomize() in the _ready() function to seed the random number generator
 func _ready():
 	randomize()
+
+
+func _on_texture_button_pressed() -> void:
+	LolloFound = true
+
+
+func _on_texture_button_2_pressed() -> void:
+	BernieFound = true
