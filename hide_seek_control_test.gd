4f7cf906f911a4e80965a1d1c97c3e2611175ca4
ChikinNoodleSupp <albin.westberg01@gmail.com>
2024-10-10 11:13:57 +0200

    i fixed transition flicker
    i just had to put hte fuckers in "on animation finished" instead of hardcoding that shit...

--- hide_seek_control_test.gd
+++ hide_seek_control_test.gd
@@ -248,15 +248,15 @@ func newScene():
 	call_deferred("mainCode")
 
 func _on_back_btn_pressed() -> void:
-	transition.play("Fade_out")
-	await get_tree().create_timer(0.6).timeout
-	get_tree().change_scene_to_file("res://MainMenu.tscn")
+	transition.play("Fade_out_2")
+	
+	
 	
 	
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "Fade_out":
+	if anim_name == "Fade_out_2":
 		print("fade out")
-		
+		get_tree().change_scene_to_file("res://MainMenu.tscn")
 		# get_tree().change_scene_to_file(sceneArray[sceneNumber%sceneArray.size()])
 		#WHY ISNT IT WORKING REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
--- main_menu.gd
+++ main_menu.gd
@@ -1,16 +1,17 @@
 extends Control
 
 @onready var transition = $AnimationPlayer
+@onready var colorrect = $AnimationPlayer/ColorRect
 
 #Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(0.2).timeout
+	colorrect.visible = true
 	transition.play("Fade_in")
 
 func _on_hide_seek_pressed() -> void:
 	transition.play("Fade_out")
-	await get_tree().create_timer(0.6).timeout
-	get_tree().change_scene_to_file("res://Hide_Seek_Main.tscn")
+	#await get_tree().create_timer(0.6).timeout
+	#get_tree().change_scene_to_file("res://Hide_Seek_Main.tscn")
 	
 	# call_deferred("_on_hide_seek_pressed")
 
@@ -20,6 +21,7 @@ func _on_puzzles_pressed() -> void:
 func _on_smoothie_mix_pressed() -> void:
 	pass # Replace with function body.
 
-#func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	#if anim_name == ("Fade_out"):
-		#get_tree().change_scene_to_file("res://Hide_Seek_Main.tscn")
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Fade_out":
+		print("Fade_out played")
+		get_tree().change_scene_to_file("res://Hide_Seek_Main.tscn")
