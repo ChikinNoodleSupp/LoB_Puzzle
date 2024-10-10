6bde29d80e575e030309dd93304744ffcd8465eb
ChikinNoodleSupp <albin.westberg01@gmail.com>
2024-10-10 10:04:51 +0200

    idk the animation broke for main scene

--- main_menu.gd
+++ main_menu.gd
@@ -4,12 +4,15 @@ extends Control
 
 #Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.2).timeout
 	transition.play("Fade_in")
 
 func _on_hide_seek_pressed() -> void:
 	transition.play("Fade_out")
+	await get_tree().create_timer(0.6).timeout
+	get_tree().change_scene_to_file("res://Hide_Seek_Main.tscn")
 	
+	# call_deferred("_on_hide_seek_pressed")
 
 func _on_puzzles_pressed() -> void:
 	pass # Replace with function body.
@@ -17,6 +20,6 @@ func _on_puzzles_pressed() -> void:
 func _on_smoothie_mix_pressed() -> void:
 	pass # Replace with function body.
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == ("Fade_out"):
-		get_tree().change_scene_to_file("res://Hide_Seek_Main.tscn")
+#func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	#if anim_name == ("Fade_out"):
+		#get_tree().change_scene_to_file("res://Hide_Seek_Main.tscn")
