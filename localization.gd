6608430397cbacb7528258dcf69075a07ce2bb37
ChikinNoodleSupp <albin.westberg01@gmail.com>
2024-10-10 14:34:42 +0200

    schtuff

--- localization.gd
+++ localization.gd
@@ -9,3 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_menu_button_pressed(id : int):
+	match id:
+		0: pass
+		1: pass
+	# if eng, localize to eng
+	# etc. etc.
+	# tutorial: https://www.youtube.com/watch?v=Lw-3Tnwv4Ds
+	# documentation: https://www.reddit.com/r/godot/comments/177rdyo/menubutton_difficult_to_use/
