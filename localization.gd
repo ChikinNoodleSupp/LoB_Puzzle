bd0d2560040e02ed4302b6c02f45d83e45605406
ChikinNoodleSupp <73000181+ChikinNoodleSupp@users.noreply.github.com>
2024-10-14 11:10:45 +0200

    idk what i did i am going to cry

--- localization.gd
+++ localization.gd
@@ -1,20 +1,14 @@
 extends Control
 
+var languageButtons = preload("res://MainMenu.tscn")
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_menu_button_pressed(id : int):
-	match id:
-		0: pass
-		1: pass
-	# if eng, localize to eng
-	# etc. etc.
-	# tutorial: https://www.youtube.com/watch?v=Lw-3Tnwv4Ds
-	# documentation: https://www.reddit.com/r/godot/comments/177rdyo/menubutton_difficult_to_use/
--- main_menu.gd
+++ main_menu.gd
@@ -2,11 +2,14 @@ extends Control
 
 @onready var transition = $AnimationPlayer
 @onready var colorrect = $AnimationPlayer/ColorRect
+@onready var button = $MarginContainer/VBoxContainer2/HBoxContainer/MenuButton
+
 
 #Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	colorrect.visible = true
 	transition.play("Fade_in")
+	button.get_popup().id_pressed.connect(_on_menu_button_pressed)
 
 func _on_hide_seek_pressed() -> void:
 	transition.play("Fade_out")
@@ -25,3 +28,14 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "Fade_out":
 		print("Fade_out played")
 		get_tree().change_scene_to_file("res://Hide_Seek_Main.tscn")
+		
+		
+func _on_menu_button_pressed(id : int):
+	match id:
+		0: String("Swedish")
+		1: String("English")
+		2: String("Danish")
+		3: String("Norwegian")
+		4: String("Finnish")
+	# tutorial: https://www.youtube.com/watch?v=Lw-3Tnwv4Ds
+	# documentation: https://www.reddit.com/r/godot/comments/177rdyo/menubutton_difficult_to_use/
