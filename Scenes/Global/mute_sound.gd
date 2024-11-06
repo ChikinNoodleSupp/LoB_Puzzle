1f4f25768704e8d1c1622c1ee7ef9a7fa08d42b3
ChikinNoodleSupp <albin.westberg01@gmail.com>
2024-11-06 11:31:29 +0100

    once again fd it up

--- Scenes/Global/mute_sound.gd
+++ Scenes/Global/mute_sound.gd
@@ -1,20 +1,23 @@
 extends Control
 
-@onready var muteSoundButton = $MuteSound
+@onready var muteSoundButton = get_node("MuteSound")
+var toggled
 
 func _ready() -> void:
 	if AudioPlayer.mute == true:
-		muteSoundButton.toggled_on = true
+		toggled = true
+		muteSoundButton.set_pressed(true)
 	elif AudioPlayer.mute == false:
-		muteSoundButton.toggled_on = false
+		toggled = false
+		muteSoundButton.set_pressed(false)
 	else:
 		pass
+	
 
 func _on_mute_sound_toggled(toggled_on: bool) -> void:
 	AudioPlayer.play_FX(AudioPlayer.clickSFX, 0)
 	if toggled_on:
 		AudioPlayer.mute = true
-		
 	else:
 		AudioPlayer.mute = false
 	AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), toggled_on)
