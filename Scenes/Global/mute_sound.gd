34fbd7f0c9eb7f730c3d4a56f04eba5b5e37a988
ChikinNoodleSupp <albin.westberg01@gmail.com>
2024-11-06 12:16:39 +0100

    idk

--- Scenes/Global/mute_sound.gd
+++ Scenes/Global/mute_sound.gd
@@ -1,9 +1,10 @@
 extends Control
 
-@onready var muteSoundButton = get_node("MuteSound")
+@onready var muteSoundButton = get_node("MuteSound") as TextureButton
 var toggled
 
 func _ready() -> void:
+	await get_tree().physics_frame
 	if AudioPlayer.mute == true:
 		toggled = true
 		muteSoundButton.set_pressed(true)
